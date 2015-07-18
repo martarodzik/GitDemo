f52a3d74a79fa9821520c170164dba6e11af1d03
appcamp <appcamp@appcamps-MacBook-Pro-2.local>
2015-07-18 09:18:44 +0200

    Initial Commit

---
+++ storyBoardDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  storyBoardDemo
+//
+//  Created by appcamp on 18/07/15.
+//  Copyright (c) 2015 appcamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
